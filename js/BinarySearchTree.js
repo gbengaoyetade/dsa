a6c798ff1d959e22393bb69072771c064524c232
gbengaPS <ioyetade@gmail.com>
2020-11-29 19:40:34 +0100

    modify binary search tree

--- js/BinarySearchTree.js
+++ js/BinarySearchTree.js
@@ -6,7 +6,6 @@ class BinarySearchTree {
   }
 
   insert(value) {
-
     if(value >= this.value) {
       if(this.right) {
         this.right.insert(value)
@@ -24,11 +23,76 @@ class BinarySearchTree {
     }
   }
 
+  search(value) {
+    if(value > this.value && this.right) {
+      return this.right.search(value)
+    }
+    if(value < this.value && this.left) {
+      return this.left.search(value)
+    }
+
+    return this.value === value ? this : null
+  }
+
   preOrder() { 
     this.value && console.log(this.value)
 
     this.left && this.left.preOrder()
 
     this.right && this.right.preOrder()
+  }
+
+  findMinimum() {
+    if(this.left) {
+      return this.left.findMinimum()
+    }
+
+    return this
+  }
+
+
+  deleteNode(value, parent=this) {
+    if(!parent){
+      return;
+    }
+
+    if(value > this.value) {
+      this.right && this.right.deleteNode(value)
+      return this;
+    }
+
+    if(value < this.value) {
+      this.left && this.left.deleteNode(value)
+      return this;
+    }
+
+    if(!parent.left && !parent.right) {
+      parent.clearNode()
+    }
+
+    if(value === this.value) {
+      if(this.right && !this.left) {
+        parent = this.right;
+        return this;
+      }
+      if(this.left && !this.right) {
+        parent = this.left;
+        return this;
+      }
+      if(this.left && this.right) {
+        const miniMum = this.right.findMinimum()
+  
+        parent.value = miniMum.value;
+        parent.right = this.right.deleteNode(miniMum.value);
+
+        return this;
+      }
+    }
+  }
+
+  clearNode() {
+    this.value = null
+    this.left = null;
+    this.right = null;
   }
 }
