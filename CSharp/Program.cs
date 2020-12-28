cbe80846ecbc4359a7f39875c9024048679189f0
Gbenga Oyetade <ioyetade@gmail.com>
2020-12-28 20:16:28 +0100

    add fibonacci method

--- CSharp/Program.cs
+++ CSharp/Program.cs
@@ -1,12 +1,30 @@
 ﻿using System;
+using System.Collections;
 
 namespace CSharp
 {
     class Program
     {
+        static Hashtable fibCache = new Hashtable();
+
+        public static int fib(int num) {
+            if(num <=2) {
+              return 1;
+            }
+
+						if(fibCache.ContainsKey(num)) {
+							return Int32.Parse(fibCache[num].ToString());
+						}
+
+            int response = fib(num -1) + fib(num -2);
+						fibCache.Add(num, response);
+
+            return response;
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine(fib(50));
         }
     }
 }
